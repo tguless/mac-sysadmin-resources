1ec301ce00e38c6c2cd354c5b94b4ff203e232c5
Ted Gulesserian <tgulesserian@rocketpharma.com>
2022-11-01 13:48:06 -0400

    Global variables not getting set.

--- scripts/cleanup/fix-onedrive-inplace.sh
+++ scripts/cleanup/fix-onedrive-inplace.sh
@@ -1,6 +1,6 @@
 cd ~
-local -r loggedinuser="$(scutil <<<"show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')"
-local -r onedrivefolder="/Users/""$loggedinuser""/OneDrive - Rocket Pharma/"
+loggedinuser="$(scutil <<<"show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')"
+onedrivefolder="/Users/""$loggedinuser""/OneDrive - Rocket Pharma/"
 
 curl -o fix-onedrive-filenames-apfs.sh https://raw.githubusercontent.com/tguless/mac-sysadmin-resources/master/scripts/cleanup/fix-onedrive-filenames-apfs.sh
 chmod u+x ./fix-onedrive-filenames-apfs.sh
