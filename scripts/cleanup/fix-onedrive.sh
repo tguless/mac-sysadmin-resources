6eebe35d7133a441218946ffccdf42ddb9a6c1b4
Ted Gulesserian <tgulesserian@rocketpharma.com>
2022-11-01 12:47:00 -0400

    when moving stuff to desktop etc make sure to add date to not overwrite older files

--- scripts/cleanup/fix-onedrive.sh
+++ scripts/cleanup/fix-onedrive.sh
@@ -6,8 +6,10 @@ function migrateOneDrive() {
    echo "Running Test"
    echo $onedrivefolder
    
-   mkdir -p "$onedrivefolder/Laptop-Documents/"
-   mkdir -p "$onedrivefolder/Laptop-Desktop/"
+   today=$(date +"%Y-%m-%d")
+
+   mkdir -p "$onedrivefolder/Laptop-Documents-${today}/"
+   mkdir -p "$onedrivefolder/Laptop-Desktop-${today}/"
 
    curl -o fix-onedrive-filenames-apfs.sh https://raw.githubusercontent.com/tguless/mac-sysadmin-resources/master/scripts/cleanup/fix-onedrive-filenames-apfs.sh
    chmod u+x ./fix-onedrive-filenames-apfs.sh
@@ -20,14 +22,16 @@ function migrateOneDrive() {
    ./fix-onedrive-filenames-apfs.sh "$documentsfolder"
    ./fix-onedrive-filenames-apfs.sh "$desktopfolder"
 
+   today=$(date +"%Y-%m-%d")
+
    cd "/Users/$loggedinuser/Desktop/"
-   find ./*  -not -path "/Users/$loggedinuser/Desktop/"Onedrive  -type d -exec mkdir -p "$onedrivefolder/Laptop-Desktop/"{} \;
-   find ./*  -not -path "/Users/$loggedinuser/Desktop/"Onedrive  -type f -exec mv {} "$onedrivefolder/Laptop-Desktop/"{} \;
+   find ./*  -not -path "/Users/$loggedinuser/Desktop/"Onedrive  -type d -exec mkdir -p "$onedrivefolder/Laptop-Desktop-${today}/"{} \;
+   find ./*  -not -path "/Users/$loggedinuser/Desktop/"Onedrive  -type f -exec mv {} "$onedrivefolder/Laptop-Desktop-${today}/"{} \;
    find ./ -depth -type d -empty -exec rmdir {} \;
 
    cd "/Users/$loggedinuser/Documents/"
-   find ./*  -not -path "/Users/$loggedinuser/Documents/"Onedrive -type d -exec mkdir -p "$onedrivefolder/Laptop-Documents/"{} \;
-   find ./*  -not -path "/Users/$loggedinuser/Documents/"Onedrive -type f -exec mv {} "$onedrivefolder/Laptop-Documents/"{} \;
+   find ./*  -not -path "/Users/$loggedinuser/Documents/"Onedrive -type d -exec mkdir -p "$onedrivefolder/Laptop-Documents-${today}/"{} \;
+   find ./*  -not -path "/Users/$loggedinuser/Documents/"Onedrive -type f -exec mv {} "$onedrivefolder/Laptop-Documents-${today}/"{} \;
    find ./ -depth -type d -empty -exec rmdir {} \;
 
 
