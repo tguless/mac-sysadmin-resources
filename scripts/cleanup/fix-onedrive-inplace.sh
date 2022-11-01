5f94fc3f48fd9fc7f8d49b4860aba827d4efc921
Ted Gulesserian <tgulesserian@rocketpharma.com>
2022-11-01 13:09:41 -0400

    Fix in place

---
+++ scripts/cleanup/fix-onedrive-inplace.sh
@@ -0,0 +1,8 @@
+cd ~
+local -r loggedinuser="$(scutil <<<"show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')"
+local -r onedrivefolder="/Users/""$loggedinuser""/OneDrive - Rocket Pharma"
+
+curl -o fix-onedrive-filenames-apfs.sh https://raw.githubusercontent.com/tguless/mac-sysadmin-resources/master/scripts/cleanup/fix-onedrive-filenames-apfs.sh
+chmod u+x ./fix-onedrive-filenames-apfs.sh
+
+./fix-onedrive-filenames-apfs.sh "${onedrivefolder}"
--- scripts/cleanup/fix-onedrive.sh
+++ scripts/cleanup/fix-onedrive.sh
@@ -13,7 +13,6 @@ function migrateOneDrive() {
 
    curl -o fix-onedrive-filenames-apfs.sh https://raw.githubusercontent.com/tguless/mac-sysadmin-resources/master/scripts/cleanup/fix-onedrive-filenames-apfs.sh
    chmod u+x ./fix-onedrive-filenames-apfs.sh
-   ./fix-onedrive-filenames-apfs.sh
 
    local -r loggedinuser="$(scutil <<<"show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')"
    local -r documentsfolder="/Users/""$loggedinuser""/Documents"
