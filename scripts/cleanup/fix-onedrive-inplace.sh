bad411ba5c91881e43a1332367d6449d965a4464
Ted Gulesserian <tgulesserian@rocketpharma.com>
2022-11-01 13:43:41 -0400

    some logging

--- scripts/cleanup/fix-onedrive-inplace.sh
+++ scripts/cleanup/fix-onedrive-inplace.sh
@@ -5,4 +5,5 @@ local -r onedrivefolder="/Users/""$loggedinuser""/OneDrive - Rocket Pharma/"
 curl -o fix-onedrive-filenames-apfs.sh https://raw.githubusercontent.com/tguless/mac-sysadmin-resources/master/scripts/cleanup/fix-onedrive-filenames-apfs.sh
 chmod u+x ./fix-onedrive-filenames-apfs.sh
 
+echo Fixing filnames in "${onedrivefolder}"
 ./fix-onedrive-filenames-apfs.sh "${onedrivefolder}"
